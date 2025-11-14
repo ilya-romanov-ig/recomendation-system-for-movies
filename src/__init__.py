12f5c2cc93691ecbbb4b81f6368421e787abbb49
root <root@DESKTOP-82M5BL7.localdomain>
2025-11-15 02:04:07 +0300

    ubcf started

---
+++ src/__init__.py
@@ -0,0 +1,3 @@
+from .data_preprocessing import create_rating_matrix, train_test_split_by_user
+
+__all__ = ['create_rating_matrix', 'train_test_split_by_user']
---
+++ src/data_preprocessing.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import numpy as np
+from scipy.sparse import csr_matrix
+from sklearn.model_selection import train_test_split
+
+def create_rating_matrix(ratings_df: pd.DataFrame):
+    user_ids = ratings_df['userId'].unique()
+    movie_ids = ratings_df['movieId'].unique()
+
+    user_to_idx = {id: idx for idx, id in enumerate(user_ids)}
+    movie_to_idx = {id: idx for idx, id in enumerate(movie_ids)}
+
+    user_indicies = ratings_df['userId'].map(user_to_idx)
+    movies_indicies = ratings_df['movieId'].map(movie_to_idx)
+    ratings = ratings_df['rating'].values
+
+    sparse_matrix = csr_matrix(
+        (ratings, (user_indicies, movies_indicies)),
+        shape=(len(user_ids), len(movie_ids))
+    )
+
+    return sparse_matrix, user_to_idx, movie_to_idx, user_ids, movie_ids
+
+def train_test_split_by_user(rating_matrix : csr_matrix, test_size=0.2, random_state=42):
+    train_data = []
+    test_data = []
+
+    coo_matrix = rating_matrix.tocoo()
+
+    for user_id in range(rating_matrix.shape[0]):
+        mask = coo_matrix.row == user_id
+        indicies = np.where(mask)[0]
+
+        if len(indicies) > 1:
+            train_idx, test_idx = train_test_split(
+                indicies,
+                random_state=random_state,
+                test_size=test_size
+            )
+
+            for idx in train_idx:
+                train_data.append((
+                    coo_matrix.row[idx],
+                    coo_matrix.col[idx],
+                    coo_matrix.data[idx]
+                ))
+        
+            for idx in test_idx:
+                test_data.append((
+                    coo_matrix.row[idx],
+                    coo_matrix.col[idx],
+                    coo_matrix.data[idx]
+                ))
+
+        if train_data:
+            row, col, data = zip(*train_data)
+            train_matrix = csr_matrix(
+                (data, (row, col)),
+                shape=rating_matrix.shape
+            )
+        else:
+            train_matrix = csr_matrix(rating_matrix.shape)
+            
+        if test_data:
+            row, col, data = zip(*test_data)
+            test_matrix = csr_matrix(
+                (data, (row, col)),
+                shape=rating_matrix.shape
+            )
+        else:
+            test_matrix = csr_matrix(rating_matrix.shape)
+
+    return train_matrix, test_matrix
